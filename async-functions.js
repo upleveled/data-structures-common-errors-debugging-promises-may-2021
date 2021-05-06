84edf95379ab1e1900b3f8decc4206c325d4d6ef
Josehower <josehowerupn@gmail.com>
2021-05-06 12:42:17 +0200

    Add fetch - async await examples

--- async-functions.js
+++ async-functions.js
@@ -1,5 +1,24 @@
 // goal 1 = print "info 1 info 2"
 // goal 2 = print "info 1 info 2" no knowing how long the timeout goes
+const number = 1;
+console.log(number);
+// await run 2 kilometers and grab my info
+const number21 = 2;
+console.log(info);
+
+fetch(www.example.com)
+  .then((res) => res.text())
+  .then((body) => {
+    console.log(body);
+  });
+
+async function myFetch() {
+  const res = await fetch(www.example.com);
+  const body = await res.text();
+
+  console.log(body);
+}
+myFetch();
 
 let info1, info2;
 
@@ -12,8 +31,6 @@ setTimeout(() => {
   info2 = 'info 2';
   run();
 }, 20);
-
-let millisecond = 0;
 
 function run() {
   if (info1 && info2) {
